ff720ba3da90b8ad5338dcb9b31960515caab4d6
Md Rakibul Islam <mdrakibulislam@Mds-MacBook-Pro.local>
2021-03-20 03:52:32 +0600

    working

--- admin/upload.php
+++ admin/upload.php
@@ -21,7 +21,7 @@
         <div class="row">
             <div class="col-lg-12">
                 <h1 class="page-header">
-                    Upload
+                    Upload 
                     <small>Subheading</small>
                 </h1>
                 <ol class="breadcrumb">
